f97373dbb49d203b0dab182721475b40ebdefbcb
Overlander <anuragnandal@hotmail.com>
2022-12-26 18:18:24 -0500

    Create characters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_26_224622) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_26_231146) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "maps", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
