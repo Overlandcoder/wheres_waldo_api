c8a7eb71db453e3d243247e293203b6bc6e7e58d
Overlander <anuragnandal@hotmail.com>
2022-12-27 08:35:26 -0500

    Add check_guess action

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,18 @@
 class CharactersController < ApplicationController
+  def check_guess
+    char_found = Character.find_char(params[:x], params[:y], params[:map])
+    # render :text => @some_object.inspect
+
+    if char_found
+      render json: { found: "test" }
+    else
+      render json: { found: "none" }
+    end
+  end
+
+  private
+
+  def character_params
+    params.permit(:x, :y, :map)
+  end
 end
