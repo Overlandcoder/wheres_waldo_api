627a683f9aa845144527e821aa9eb4f00fed62b7
Overlander <anuragnandal@hotmail.com>
2022-12-27 08:35:11 -0500

    Add find_char method

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ApplicationRecord
   belongs_to :map
+
+  def find_char(guess_x, guess_y, map)
+    map_id = Map.find_by(name: map).id
+    characters = Character.where(map: map_id).pluck(:name, :position)
+
+    characters.any? do |character|
+      x1 = character.position[0]
+      x2 = character.position[1]
+      y1 = character.position[2]
+      y2 = character.position[3]
+
+      return character.name if guess_x.between?(x1, x2) && guess_y.between?(y1, y2)
+    end
+  end
 end
