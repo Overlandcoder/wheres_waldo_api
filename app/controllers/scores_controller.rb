e5d3f9220e4bc0f8858dc8d55926eed83760885f
Overlander <anuragnandal@hotmail.com>
2022-12-27 22:01:20 -0500

    Add Scores#create action to save scores and render JSON

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,17 @@
 class ScoresController < ApplicationController
+  def create
+    @score = Score.new(score_params)
+
+    if @score.save
+      render json: { message: "saved" }
+    else
+      render json: { message: @score.errors }
+    end
+  end
+
+  private
+
+  def score_params
+    params.permit(:name, :seconds)
+  end
 end
