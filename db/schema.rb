a96398d395c7efe480128d5c862f6a6319d3931d
Overlander <anuragnandal@hotmail.com>
2022-12-27 19:55:35 -0500

    Generate scores table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_194005) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_005356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_194005) do
 
   create_table "maps", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.string "name"
+    t.integer "seconds"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
