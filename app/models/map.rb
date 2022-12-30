78687d1150e964f8f30f7fd1563b85901315b5d2
Overlander <anuragnandal@hotmail.com>
2022-12-30 08:10:29 -0500

    Make scores belong to maps

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,4 @@
 class Map < ApplicationRecord
   has_many :characters
+  has_many :scores
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ApplicationRecord
+  belongs_to :map  
   validates :name, length: { maximum: 15, too_long: "can't be longer than 15 characters" }
-
   scope :ordered, -> { order(seconds: :asc) }
 end
