fa70fc11156cf35112d02c5a5f8a20684ba3087e
Overlander <anuragnandal@hotmail.com>
2022-12-30 20:44:25 -0500

    Add seeds for ski map

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ beach.characters.create(name: "waldo", position: [0.7263, 0.7326, 0.4283, 0.4563
 beach.characters.create(name: "wizard", position: [0.3767, 0.3872, 0.4058, 0.4337])
 beach.characters.create(name: "odlaw", position: [0.2159, 0.2216, 0.4076, 0.4292])
 beach.characters.create(name: "wilma", position: [0.8808, 0.8877, 0.4617, 0.477])
+
+ski = Map.create(name: "ski")
+ski.characters.create(name: "waldo", position: [0.9606, 0.9716, 0.8642, 0.9228])
+ski.characters.create(name: "wizard", position: [0.1759, 0.1806, 0.9023, 0.9300])
+ski.characters.create(name: "odlaw", position: [0.4248, 0.4329, 0.7665, 0.7840])
+ski.characters.create(name: "wilma", position: [0.5961, 0.6059, 0.5195, 0.5463])
