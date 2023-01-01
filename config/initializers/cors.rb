4ceabe3f9928c306afb273173cc43a119b4ec418
Overlander <anuragnandal@hotmail.com>
2023-01-01 07:18:16 -0500

    Attempt to fix cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,6 +18,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 end
 
-Rails.application.config.hosts << "http://localhost:3001"
-Rails.application.config.hosts << "https://overlandcoder.github.io/wheres_waldo/"
-Rails.application.config.hosts << "https://wheres-waldo-service.onrender.com"
+# Rails.application.config.hosts << "http://localhost:3001"
+# Rails.application.config.hosts << "https://overlandcoder.github.io/wheres_waldo/"
+# Rails.application.config.hosts << "https://wheres-waldo-service.onrender.com"
+
+Rails.application.configure do
+  config.hosts.clear
+end
