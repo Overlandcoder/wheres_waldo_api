284f5633df4c38e1fc465b25459fa61fa0fa1f36
Overlander <anuragnandal@hotmail.com>
2023-01-01 17:06:28 -0500

    Attempt to fix score retrieval

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -14,7 +14,9 @@ class ScoresController < ApplicationController
     @top_scores = {}
 
     Map.find_each do |map|
-      puts maps.scores.count
+      puts Map.count
+      puts "#{map} score count: #{map.scores.count}"
+      p params
       @top_scores[map.name] = map.scores.ordered.limit(params[:amount].to_i)
     end
 
