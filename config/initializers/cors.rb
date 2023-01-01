29387aafe91d298afb9869d6e03afa2cba8b33a5
Overlander <anuragnandal@hotmail.com>
2023-01-01 08:11:37 -0500

    Allow all origins for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://overlandcoder.github.io/wheres-waldo",
-            "https://wheres-waldo-service.onrender.com",
-            "http://localhost:3001",
-            "127.0.0.1:3001"
+    origins "*"
 
     resource "*",
       headers: :any,
